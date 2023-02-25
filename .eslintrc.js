3916def5629efc18fe9e69a06cb3ea961a4f53b7
yougun <yequn0728@gmail.com>
2023-02-25 18:02:23 +0800

    完成了登录功能

--- .eslintrc.js
+++ .eslintrc.js
@@ -12,6 +12,19 @@ module.exports = {
   },
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off'
-  }
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'vue/multi-word-component-names': 'off',
+    'space-before-function-paren': 0
+  },
+  overrides: [
+    {
+      files: [
+        '**/__tests__/*.{j,t}s?(x)',
+        '**/tests/unit/**/*.spec.{j,t}s?(x)'
+      ],
+      env: {
+        jest: true
+      }
+    }
+  ]
 }
