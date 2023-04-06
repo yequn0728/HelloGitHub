a46ab5b262c94e38dd15400c9898222ab6946b1d
yougun <yequn0728@gmail.com>
2023-04-06 08:35:57 +0800

    完成订单功能的开发

--- src/plugins/element.js
+++ src/plugins/element.js
@@ -1,5 +1,7 @@
 import Vue from 'vue'
 import { Upload, CheckboxGroup, Checkbox, Steps, Step, TabPane, Tabs, Alert, Cascader, Select, Option, Tree, Tag, MessageBox, Dialog, Pagination, Tooltip, Switch, Table, TableColumn, Col, Row, Card, Breadcrumb, BreadcrumbItem, Menu, Submenu, MenuItem, Button, Form, FormItem, Input, Message, Container, Header, Aside, Main } from 'element-ui'
+import Timeline from './timeline/index.js'
+import TimelineItem from './timeline-item/index.js'
 Vue.use(Button)
 Vue.use(Form)
 Vue.use(FormItem)
@@ -36,6 +38,8 @@ Vue.use(Step)
 Vue.use(CheckboxGroup)
 Vue.use(Checkbox)
 Vue.use(Upload)
+Vue.use(Timeline)
+Vue.use(TimelineItem)
 
 Vue.prototype.$message = Message
 Vue.prototype.$confirm = MessageBox.confirm
